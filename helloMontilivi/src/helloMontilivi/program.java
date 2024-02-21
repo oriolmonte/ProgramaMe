c9feb420752f4f9cd177b4614826fa0af09cb3a6
Oriol Monte <oriol.monte95@gmail.com>
2024-02-21 20:05:14 +0100

    first commit

---
+++ helloMontilivi/src/helloMontilivi/program.java
@@ -0,0 +1,10 @@
+package helloMontilivi;
+
+public class program {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hola Montilivi, em dic Oriol Monte!");
+	}
+
+}
---
+++ helloMontilivi/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module helloMontilivi {
+}
